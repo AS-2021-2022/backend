3064ec2772301b1ce6d6d48e97c9f656265de062
Joao-Felisberto <joaofelisberto@ua.pt>
2022-06-08 22:21:38 +0100

    Attempting to fix requests.

--- src/main/java/com/github/as2122/backend/BackendApplication.java
+++ src/main/java/com/github/as2122/backend/BackendApplication.java
@@ -13,6 +13,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @SpringBootApplication
@@ -44,6 +47,16 @@ public class BackendApplication {
 	@Bean
 	public TeamManager teamManager() {
 		return new TeamManager(accountManager());
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
 	}
 
 	public static void main(String[] args) {
